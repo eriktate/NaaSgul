a6885036f5cb68a2888d0dd82759e62cbf85b7c6
Erik Tate <erik.tate@blackbaud.com>
2016-05-01 16:51:24 -0400

    Setting up most basic webserver

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+    "net/http"
+    "log"
+
+    "github.com/gorilla/mux"
+)
 
 func main() {
-    fmt.Println("Testing")
+    router := mux.NewRouter()
+
+    router.HandleFunc("/", basicHandler)
+
+    log.Panic(http.ListenAndServe("localhost:1337", router))
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello world!"))
 }
