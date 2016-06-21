390371e6a84cb70a2e9230acb10df65beb61eb8e
Erik Tate <erik.tate@blackbaud.com>
2016-06-20 21:17:40 -0400

    Fixing issues with unit tests caused by refactoring and renaming models/entities

--- entities/pushnotification_test.go
+++ entities/pushnotification_test.go
@@ -2,7 +2,6 @@ package entities
 
 import (
 	"testing"
-	"time"
 
 	"github.com/satori/go.uuid"
 	. "github.com/smartystreets/goconvey/convey"
@@ -16,7 +15,7 @@ func TestCreatingPushNotifications(t *testing.T) {
 
 		notification := NewNotification(subject, body, notifType)
 		Convey("When NewPushNotification is called", func() {
-			pushNotification := NewPushNotification(*notification)
+			pushNotification := NewPushNotification(notification)
 
 			Convey("Then a new PushNotification entity should be returned", func() {
 				So(pushNotification, ShouldNotBeNil)
@@ -28,7 +27,7 @@ func TestCreatingPushNotifications(t *testing.T) {
 
 func TestPushNotificationGetters(t *testing.T) {
 	Convey("Given a valid PushNotification entity", t, func() {
-		pushNotification := NewPushNotification(*NewNotification("testing", "test body", Text))
+		pushNotification := NewPushNotification(NewNotification("testing", "test body", Text))
 		pushNotification.SetSubscriberID("b3a8ab73-41e1-4038-a27d-25f3ea64dcfc")
 		pushNotification.SetSubscriberClientID("12345")
 		pushNotification.SetHasBeenSeen(true)
@@ -60,73 +59,46 @@ func TestPushNotificationGetters(t *testing.T) {
 }
 
 func TestPushNotificationSetters(t *testing.T) {
-	Convey("Given a valid Notification entity", t, func() {
-		notification, _ := BuildNotification(uuid.NewV1().String(), "testing", "test body", "text", time.Now())
+	Convey("Given a valid PushNotification entity", t, func() {
+		pushNotification := NewPushNotification(NewNotification("testing", "test body", Text))
+		pushNotification.SetSubscriberID("b3a8ab73-41e1-4038-a27d-25f3ea64dcfc")
+		pushNotification.SetSubscriberClientID("12345")
+		pushNotification.SetHasBeenSeen(true)
 
-		Convey("When attempting to set the notificationID with a UUID", func() {
+		Convey("When attempting to set the embedded Notification", func() {
+			notification := NewNotification("NewTest", "New body", HTML)
+
+			pushNotification.SetNotification(notification)
+			Convey("Then the embedded fields should be updated", func() {
+				So(pushNotification.Subject(), ShouldEqual, "NewTest")
+				So(pushNotification.NotificationType(), ShouldEqual, HTML)
+			})
+		})
+
+		Convey("When attempting to set the subscriberID", func() {
 			id := uuid.NewV1()
-			notification.SetNotificationID(id)
+			pushNotification.SetSubscriberID(id.String())
 
-			Convey("Then the notificationID should be updated", func() {
-				So(notification.NotificationID(), ShouldEqual, id)
+			Convey("Then the subscriberID should be updated", func() {
+				So(pushNotification.SubscriberID(), ShouldEqual, id)
 			})
 		})
 
-		Convey("When attempting to build the notificationID", func() {
-			Convey("with a valid UUID string", func() {
-				id := "6ba7b810-9dad-11d1-80b4-00c04fd430c8"
-				err := notification.BuildNotificationID(id)
+		Convey("When attempting to set the subscriberClientID", func() {
+			id := "9876"
+			pushNotification.SetSubscriberClientID(id)
 
-				Convey("Then the notificationID should be updated and no errors returned", func() {
-					So(err, ShouldBeNil)
-					So(notification.NotificationID().String(), ShouldEqual, id)
-				})
-			})
-
-			Convey("with an invalid UUID string", func() {
-				id := "this is bad"
-				err := notification.BuildNotificationID(id)
-
-				Convey("Then the notificationID should not be updated and an error should be returned", func() {
-					So(err, ShouldNotBeNil)
-					So(notification.NotificationID(), ShouldNotEqual, id)
-				})
+			Convey("Then the subscriberClientID should be updated", func() {
+				So(pushNotification.SubscriberClientID(), ShouldEqual, id)
 			})
 		})
 
-		Convey("When attempting to set the subject", func() {
-			subject := "new subject"
-			notification.SetSubject(subject)
+		Convey("When attempting to set the hasBeenSeen flag", func() {
+			hasBeenSeen := false
+			pushNotification.SetHasBeenSeen(hasBeenSeen)
 
-			Convey("Then the subject should be updated", func() {
-				So(notification.Subject(), ShouldEqual, subject)
-			})
-		})
-
-		Convey("When attempting to set the body", func() {
-			body := "new body"
-			notification.SetBody(body)
-
-			Convey("Then the body should be updated", func() {
-				So(notification.Body(), ShouldEqual, body)
-			})
-		})
-
-		Convey("When attempting to set the notificationType", func() {
-			notifType := HTML
-			notification.SetNotificationType(notifType)
-
-			Convey("Then the notificationType should be updated", func() {
-				So(notification.NotificationType(), ShouldEqual, notifType)
-			})
-		})
-
-		Convey("When attempting to set the createDate", func() {
-			createDate := time.Now()
-			notification.SetCreateDate(createDate)
-
-			Convey("Then the createDate should be updated", func() {
-				So(notification.CreateDate().Equal(createDate), ShouldBeTrue)
+			Convey("Then the hasBeenSeen flag should be updated", func() {
+				So(pushNotification.HasBeenSeen(), ShouldBeFalse)
 			})
 		})
 	})
--- services/notificationservice_test.go
+++ services/notificationservice_test.go
@@ -19,20 +19,20 @@ func TestNotificationService(t *testing.T) {
 
 		notificationService := NewNotificationService(mockRepo)
 
-		Convey("And a valid NotificationDTO", func() {
-			notificationDTO := &models.NotificationDTO{
+		Convey("And a valid Notification", func() {
+			Notification := &models.Notification{
 				Subject:          "Test",
 				Body:             "Test",
 				NotificationType: "text",
 				CreateDate:       time.Now(),
 			}
 
-			newDTO := *notificationDTO
+			newDTO := *Notification
 			newDTO.NotificationID = "c48feb4f-44c1-4e83-8b1b-fb1408f0db28"
 
 			Convey("When we attempt to create a notification", func() {
 				mockRepo.EXPECT().CreateNotification(gomock.Any()).Return(&newDTO, nil).MinTimes(1)
-				returnedDTO, err := notificationService.CreateNotification(notificationDTO)
+				returnedDTO, err := notificationService.CreateNotification(Notification)
 
 				Convey("The NotificationRepo should be called and return no errors", func() {
 					So(returnedDTO.NotificationID, ShouldEqual, newDTO.NotificationID)
@@ -40,8 +40,8 @@ func TestNotificationService(t *testing.T) {
 				})
 			})
 
-			Convey("And an invalid NotificationDTO", func() {
-				notificationDTO := &models.NotificationDTO{
+			Convey("And an invalid Notification", func() {
+				Notification := &models.Notification{
 					NotificationID:   "bad ID",
 					Subject:          "Test",
 					Body:             "Test",
@@ -50,11 +50,11 @@ func TestNotificationService(t *testing.T) {
 				}
 
 				Convey("When we attempt to create a notification", func() {
-					mockRepo.EXPECT().CreateNotification(gomock.Any()).Return(notificationDTO, nil).MaxTimes(0)
-					returnedDTO, err := notificationService.CreateNotification(notificationDTO)
+					mockRepo.EXPECT().CreateNotification(gomock.Any()).Return(Notification, nil).MaxTimes(0)
+					returnedDTO, err := notificationService.CreateNotification(Notification)
 
 					Convey("Then the NotificationRepo should not be called, the original DTO should be returned, and an error should be returned", func() {
-						So(returnedDTO, ShouldEqual, notificationDTO)
+						So(returnedDTO, ShouldEqual, Notification)
 						So(err, ShouldNotBeNil)
 					})
 				})
@@ -65,7 +65,7 @@ func TestNotificationService(t *testing.T) {
 					notificationID := "c48feb4f-44c1-4e83-8b1b-fb1408f0db28"
 					uuidNotificationID, _ := uuid.FromString(notificationID)
 					Convey("When we call GetNotificationByID", func() {
-						mockRepo.EXPECT().GetNotificationByID(uuidNotificationID).Return(&models.NotificationDTO{}, nil).MinTimes(1)
+						mockRepo.EXPECT().GetNotificationByID(uuidNotificationID).Return(&models.Notification{}, nil).MinTimes(1)
 						_, err := notificationService.GetNotificationByID(notificationID)
 
 						Convey("Then the NotificationRepo should be called and no errors returned", func() {
@@ -76,7 +76,7 @@ func TestNotificationService(t *testing.T) {
 				Convey("When given a UUID NotificationID", func() {
 					notificationID := uuid.NewV1()
 					Convey("When we call GetNotificationbyID", func() {
-						mockRepo.EXPECT().GetNotificationByID(notificationID).Return(&models.NotificationDTO{}, nil).MinTimes(1)
+						mockRepo.EXPECT().GetNotificationByID(notificationID).Return(&models.Notification{}, nil).MinTimes(1)
 						_, err := notificationService.GetNotificationByID(notificationID)
 
 						Convey("Then the NotificationRepo should be called and no errors returned", func() {
@@ -108,7 +108,7 @@ func TestNotificationService(t *testing.T) {
 				})
 				Convey("When given a subject that is at least 5 characters", func() {
 					subject := "Testing"
-					notifications := make([]*models.NotificationDTO, 12)
+					notifications := make([]*models.Notification, 12)
 
 					Convey("When we call GetNotificationsBySubject", func() {
 						mockRepo.EXPECT().GetNotificationsBySubject(subject).Return(notifications).MinTimes(1)
