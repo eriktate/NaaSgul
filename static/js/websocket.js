0ce9fdcd78b7fe27200b2e270f4775d53237f839
Erik Tate <erik.tate@blackbaud.com>
2016-10-16 11:25:17 -0400

    Resetting for a complete refactor

--- static/js/websocket.js
+++
@@ -1,9 +0,0 @@
-var testSocket = new WebSocket("ws://api.naasgul.io/ws");
-
-testSocket.onopen = function(event) {
-    testSocket.send("THIS IS A TEST!");
-}
-
-testSocket.onmessage = function(event) {
-    console.log(event.data);
-}
